a411372dbc73740ddb916aa2b54b772201bc47bc
Eli69Pro <elaheh.golrokh@gmail.com>
2025-08-18 12:51:11 +0100

    move final models dictionary to config

--- train.py
+++ train.py
@@ -1,5 +1,23 @@
+import argparse
 import pandas as pd
+import yaml
+
+from omegaconf import OmegaConf
+
 from src.training import ModelingPipeline
+
+
+# parser = argparse.ArgumentParser(
+#     prog='train.py',
+#     description='Modeling pipeline on the preprocessed data',
+#     epilog=f'Thanks for using.'
+# )
+
+# parser.add_argument('--', help='set the training data file path')
+# args = parser.parse_args()
+
+config = OmegaConf.load('config.yaml')
+
 
 def main():
     train_scaled = pd.read_csv('data/preprocessed_train_df.csv')
@@ -13,11 +31,13 @@ def main():
     print(X_train.shape, y_train.shape)
     print(X_test.shape, y_test.shape)
 
-    pipeline = ModelingPipeline(X_train,
-                  y_train,
-                  X_test,
-                  y_test,
-                  logging_flag=True)
+    pipeline = ModelingPipeline(config,
+                                X_train,
+                                y_train,
+                                X_test,
+                                y_test,
+                                default_model=None,
+                                )
     pipeline.run_pipeline()
     print(f'pipeline.best_feature_counts_ : {pipeline.best_feature_counts_}')
 
