2d0b5de542ca7f98647eab3ecd65e58bd5c6b532
Eli69Pro <elaheh.golrokh@gmail.com>
2025-08-29 16:37:21 +0100

    Fetch secret vars from environment variables

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from omegaconf import OmegaConf
+import os
 
 from src.serving import GradioApp
 
 
-config = OmegaConf.load('config.yaml')
+base_conf = OmegaConf.load('config.yaml')
+private_conf = OmegaConf.create({
+    "repo_id": os.getenv("REPO_ID"),
+    "hf_token": os.getenv("HF_TOKEN")
+})
+config = OmegaConf.merge(base_conf, private_conf)
 
 app = GradioApp(config)
 demo = app.build()
